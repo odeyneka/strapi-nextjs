3d010ebe0353bc6b53ccdf139e5d837b7e185dbb
Oleh Deyneka <o.deyneka@univisia.com>
2024-04-19 19:56:03 +0300

    clear

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1", "https://smiling-deer-58ce9951c7.media.strapiapp.com", "smiling-deer-58ce9951c7.media.strapiapp.com"],
+    domains: ["127.0.0.1", "smiling-deer-58ce9951c7.media.strapiapp.com"],
   },
   env: {
     STRAPI_API_URL: process.env.STRAPI_API_URL,
--- src/api/getImage.ts
+++ src/api/getImage.ts
@@ -1,8 +1,6 @@
 export const getImage = (data: any) => {
   const url: string = data.attributes.Image.data?.attributes?.url;
   const imageUrl = url.startsWith('https') ? url : `${process.env.STRAPI_API_URL}${url}`;
-  console.log(url);
-  console.log( imageUrl);
 
   return imageUrl;
 }
